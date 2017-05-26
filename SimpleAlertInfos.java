3de3f394169c05d7e1b1905e85f7093efa9f33d5
mevato_minecraft <simon56890@gmail.com>
2017-05-26 15:28:05 +0200

    Update SimpleAlertInfos.java

--- SimpleAlertInfos.java
+++ SimpleAlertInfos.java
@@ -1,1 +1,26 @@
+package fr.mevato.SimpleAlertInfos;
 
+import org.bukkit.plugin.java.JavaPlugin;
+
+import fr.mevato.SimpleAlertInfos.commands.CommandBCA;
+import fr.mevato.SimpleAlertInfos.commands.CommandBCI;
+
+/**
+ * Version 1.2 created by mevato on 18/05/2017.
+ */
+
+public class SimpleAlertInfos extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        System.out.println("[SimpleAlertInfos] Enabled");
+        System.out.println("[SimpleAlertInfos] Want a french version ? Go To The Spigot Page !");
+        getCommand("bci").setExecutor(new CommandBCI());
+        getCommand("bca").setExecutor(new CommandBCA());
+    }
+
+    @Override
+    public void onDisable() {
+        System.out.println("[SimpleAlertInfos] Disabled");
+    }
+}
